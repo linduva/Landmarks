0e477e7db88b0b2cbc8925766db789e04749dec6
linduva <bdiemer@linduva.com>
2022-01-06 12:05:25 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Bernhard Diemer  on 06.01.22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
